0adc7f33266987e69aa35811bc062e30cc753bec
Kian <122436242+kian-al@users.noreply.github.com>
2025-10-02 02:48:51 +0330

    Add Food model with id, name, and price fields

---
+++ src/main/java/org/TaskManagerProgram/com/Models/Food.java
@@ -0,0 +1,13 @@
+package org.TaskManagerProgram.com.Models;
+
+public class Food {
+    public Integer id;
+    public String name;
+    public Double price;
+
+    public Food(Integer id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
