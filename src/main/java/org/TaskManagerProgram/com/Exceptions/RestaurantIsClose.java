53e6e386a7b3a66b5c59965f1145646f33db2fd6
Kian <122436242+kian-al@users.noreply.github.com>
2025-10-02 11:54:30 +0330

    Add RestaurantIsClose exception for closed restaurant cases

---
+++ src/main/java/org/TaskManagerProgram/com/Exceptions/RestaurantIsClose.java
@@ -0,0 +1,7 @@
+package org.TaskManagerProgram.com.Exceptions;
+
+public class RestaurantIsClose extends Throwable{
+    public RestaurantIsClose(){
+        System.out.println("Restaurant is closed");
+    }
+}
